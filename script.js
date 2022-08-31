eeeaae05a055407d30c0d91ba39c0e2b474ca2ea
Devang2304 <devangvartak2304@gmail.com>
2022-08-31 20:03:36 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let inputDir={x:0,y:0};
+const foodSound= new Audio('');
+const gameOverSound=new Audio('');
+const moveSound=new Audio('');
+const musicSound=new Audio('');
+let speed=5;
+let score=0;
+let lastPaintTime=0;
+let snakeArray=[
+    {x:13,y:15}
+]
